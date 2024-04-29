68924cd6ac32ba1cf784d6637012bc7ca2f3dbfd
CDB180\BE76820 <decoster.adinda@gmail.com>
2024-04-29 18:04:36 +0200

    script to create df from list of json files

---
+++ Scripts/df_from_json_files.py
@@ -0,0 +1,13 @@
+# Functie die DataFrame "df" creëert vanuit .json files die zich in een folder "directory" bevinden
+
+def df_from_json_files (directory) :
+    
+    data_list = []
+
+    for file in os.listdir(directory) :
+        with open(file) as file:
+            data = json.loads(file.read())
+        data_list.append(data)
+
+    df = pd.DataFrame(data_list)
+    return df  
