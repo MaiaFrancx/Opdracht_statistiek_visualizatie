ae036502bb4312fec5076a21681fb1bb9c007828
Qrt69 <35011532+Qrt69@users.noreply.github.com>
2024-05-18 19:17:16 +0200

    Add files via upload

--- Scripts/Tools.py
+++ Scripts/Tools.py
@@ -78,7 +78,7 @@ def plot_histogram(df, column, axp, title='', binsp=50, alphap=0.5, colorp='b', 
 def model_prod(p, mu, sigma):
     """
     Function calculates the production for one day
-    The chance of having zere production is p
+    The chance of having zero production is p
     So there's a (1 - p) chance that there is production
     The mu parameter is the mean value
     The sigma parameter is the standard deviation
@@ -107,5 +107,41 @@ def plot_ecdf(ecdf, ax, theo_max, title='ECDF'):
     ax.set_ylabel('ECDF')
     for quartile in range(1, 4):
         ax.axvline(x = theo_max * quartile * 0.25, color = 'r', linestyle = '--' )
+    ax.fill_between(ecdf.x, ecdf.y, ecdf.y - ecdf.y, color = 'm', alpha=0.2)
     ax.grid(True)
+
+
+def generate_summed_data(perc, model_params, days, num_samples=1000):
+    """
+    Generates summed data for a specified number of days.
+    :param perc: percentage of zero production, used to generate simulated production
+    :param model_params: list containing mean and standard deviation
+    :param days: number of days to generate
+    :param num_samples: number of samples to generate
+    """
+
+    return [sum([model_prod(perc, model_params[0], model_params[1]) for _ in range(days)]) for _ in range(num_samples)]
+
+
+def compare_ecdf_to_normal(data, ax, num_samples):
+    # Calculate ECDF for the given data
+    ecdf = ECDF(data)
+
+    # Generate points on the x axis from the min to max of observed data
+    x = np.linspace(min(data), max(data), 100)
+
+    # Calculate the CDF of a normal distribution with the same mean and std as the data
+    cdf = norm.cdf(x, np.mean(data), np.std(data))
+
+    # Plot the ECDF and the theoretical normal CDF
+
+    #    ax.plot(ecdf.x, ecdf.y, linestyle= 'solid', marker='.', markersize=1, label='ECDF')
+    ax.plot(ecdf.x, ecdf.y, '-r', linewidth=2, label='ECDF')
+    ax.plot(x, cdf, label='Normal CDF')
+    ax.fill_between(x, cdf, color='skyblue', alpha=0.4)
+    # Setting labels and titles
+    ax.set_title(f'Comparison with n={num_samples}')
+    ax.set_xlabel('Production')
+    ax.set_ylabel('Cumulative Probability')
+    ax.legend()
 
